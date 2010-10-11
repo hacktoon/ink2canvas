fa8bacec3b90d27c35af56789bbbb4533399e09e
PotHix <pothix@pothix.com>
2010-10-11 02:11:16 -0300

    Adding test directory

---
+++ test/canvas_test.py
@@ -0,0 +1,13 @@
+import sys
+import unittest
+
+sys.path.append('..')
+import canvas
+
+class TestSequenceFunctions(unittest.TestCase):
+  def test_truth(self):
+    """docstring for the truth"""
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
