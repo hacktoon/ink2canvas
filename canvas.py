03e4c8bb4a1d30955e202cdecc94076f9b2b539a
PotHix <pothix@pothix.com>
2010-10-11 01:41:31 -0300

    Removing trailing spaces Fixing typo on method name

--- canvas.py
+++ canvas.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 '''
 Copyright (C) 2009 Karlisson Bezerra, contato@nerdson.com
 
@@ -23,48 +23,43 @@ import re
 from simplestyle import parseStyle
 from simplepath import parsePath
 
-#overwrite debug metohd
+#overwrite debug method
 log = inkex.debug
 
 class Canvas(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
-        
         self.code = ""
-    
-    
+
     def out(self, text):
         self.code += text + "\n"
-    
-    
+
     def rgb(self, h):
         h = h[1:]
         r = int(h[:2], 16)
         g = int(h[2:4], 16)
         b = int(h[4:], 16)
         return r, g, b
-    
-    
+
     def set_style(self, style):
         """Sets the style methods"""
-    
+
         #stroke properties
         if style.has_key("stroke-width"):
             self.out("ctx.lineWidth = " + inkex.unittouu(style["stroke-width"]) + ";")
-        
+
         if style.has_key("stroke-linecap"):
             self.out("ctx.lineCap = " + style["stroke-linecap"] + ";")
-        
+
         if style.has_key("stroke-linejoin"):
             self.out("ctx.lineJoin = " + style["stroke-linejoin"] + ";")
-        
+
         if style.has_key("stroke-miterlimit"):
             self.out("ctx.miterLimit = " + style["stroke-miterlimit"] + ";")
 
         if style.has_key("opacity"):
             if int(style["opacity"]) < 1:
                 self.out("ctx.globalAlpha = " + style["opacity"] + ";")
-
 
         # stroke color
         if style.has_key("stroke"):  # stroke may be 'unset'
@@ -78,8 +73,7 @@ class Canvas(inkex.Effect):
                         self.out("ctx.strokeStyle = 'rgba(%d, %d, %d, %d)';" % (r, g, b, alpha))
                 else:
                     self.out("ctx.strokeStyle = 'rgb(%d, %d, %d)';" % (r, g, b))
-        
-        
+
         # fill color
         if style.has_key("fill"): # fill may be 'unset'
             if style["fill"] != "none":
@@ -92,21 +86,20 @@ class Canvas(inkex.Effect):
                         self.out("ctx.fillStyle = 'rgba(%d, %d, %d, %d)';" % (r, g, b, alpha))
                 else:
                     self.out("ctx.fillStyle = 'rgb(%d, %d, %d)';" % (r, g, b))
-    
-    
+
     def effect(self):
         """Applies the effect"""
-        
+
         self.out("ctx = canvas.getContext(\"2d\");")
-        
+
         for nid, node in self.selected.iteritems():
             self.out("// " + node.get("id"))
             self.out("ctx.beginPath();\n")
-            
+
             #parse the element style properties into a dictionary
             style = parseStyle(node.get("style"))
             self.set_style(style)
-            
+
             #start the drawing
             path = parsePath(node.get("d"))
             for p in path:
@@ -114,30 +107,26 @@ class Canvas(inkex.Effect):
                     self.out("ctx.moveTo(%d, %d);" % (p[1][0], p[1][1]))
                 elif p[0] == "L":
                     self.out("ctx.lineTo(%d, %d);" % (p[1][0], p[1][1]))
-            
-            
+
             #finish the path
             if style.has_key("stroke"):
                 self.out("\nctx.stroke();")
-                
+
             if style.has_key("fill"):
                 self.out("\nctx.fill();")
-            
-            
+
             self.out("\nctx.closePath();")
-            
+
             #reseting globalAlpha value
             if style.has_key("opacity"):
                 if int(style["opacity"]) < 1:
                     self.out("ctx.globalAlpha = 1;")
-            
+
             #if node.tag == inkex.addNS("text", "svg"):
-        
-        
+
         f = open("canvas.js", "w")
         f.write(self.code)
         f.close()
-                
 
 b = Canvas()
-b.affect()
+b.effect()
