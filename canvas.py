59c3d53919bf5fcb6637e11d98462d96a837065b
Karlisson Bezerra <theanimage@gmail.com>
2010-10-12 11:35:35 -0300

    Made several modifications

--- canvas.py
+++ canvas.py
@@ -19,11 +19,12 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import inkex
 
-from lib.simplestyle import parseStyle
-from lib.simplepath import parsePath
+from simplestyle import parseStyle
+from simplepath import parsePath
 
 #overwrite debug method
 log = inkex.debug
+
 
 class Canvas(inkex.Effect):
     def __init__(self):
@@ -31,62 +32,55 @@ class Canvas(inkex.Effect):
         self.code = ""
 
     def out(self, text):
+        """Stores text to be written to a file later"""
         self.code += text + "\n"
 
-    def rgb(self, h):
-        h = h[1:]
-        r = int(h[:2], 16)
-        g = int(h[2:4], 16)
-        b = int(h[4:], 16)
-        return r, g, b
+    def set_color(self, rgb, a):
+        """Returns a rgba set"""
+        rgb = rgb[1:]  # removes the '#'
+        r = int(rgb[:2], 16)
+        g = int(rgb[2:4], 16)
+        b = int(rgb[4:], 16)
+        a = float(a)
+        if a == 1.0:
+            return "'rgb(%d, %d, %d)';" % (r, g, b)
+        return "'rgba(%d, %d, %d, %.1f)';" % (r, g, b, a)
 
     def set_style(self, style):
         """Sets the style methods"""
+        if style.has_key("opacity") and float(style["opacity"]) < 1:
+            self.out("ctx.globalAlpha = %.1f;" % float(style["opacity"]))
 
         #stroke properties
         if style.has_key("stroke-width"):
             self.out("ctx.lineWidth = %d;" % inkex.unittouu(style["stroke-width"]))
 
         if style.has_key("stroke-linecap"):
-            self.out("ctx.lineCap = %d;" % style["stroke-linecap"])
+            self.out("ctx.lineCap = '%s';" % style["stroke-linecap"])
 
         if style.has_key("stroke-linejoin"):
-            self.out("ctx.lineJoin = %d;" % style["stroke-linejoin"])
+            self.out("ctx.lineJoin = '%s';" % style["stroke-linejoin"])
 
         if style.has_key("stroke-miterlimit"):
-            self.out("ctx.miterLimit = %d;" % style["stroke-miterlimit"])
-
-        if style.has_key("opacity") and int(style["opacity"]) < 1:
-            self.out("ctx.globalAlpha = %d;" % style["opacity"])
+            self.out("ctx.miterLimit = %s;" % style["stroke-miterlimit"])
 
         # stroke color
-        if style.has_key("stroke") and style["stroke"] != "none": # stroke may be 'unset'
-            r, g, b = self.rgb(style["stroke"])
+        if style.has_key("stroke") and style["stroke"] != "none":
+            alpha = 1
             if style.has_key("stroke-opacity"):
-                alpha = int(style["stroke-opacity"])
-                if alpha == 1:
-                    self.out("ctx.strokeStyle = 'rgb(%d, %d, %d)';" % (r, g, b))
-                else:
-                    self.out("ctx.strokeStyle = 'rgba(%d, %d, %d, %d)';" % (r, g, b, alpha))
-            else:
-                self.out("ctx.strokeStyle = 'rgb(%d, %d, %d)';" % (r, g, b))
+                alpha = style["stroke-opacity"]
+            self.out("ctx.strokeStyle = " + self.set_color(style["stroke"], alpha))
 
         # fill color
-        if style.has_key("fill"): # fill may be 'unset'
-            if style["fill"] != "none":
-                r, g, b = self.rgb(style["fill"])
-                if style.has_key("fill-opacity"):
-                    alpha = int(style["fill-opacity"])
-                    if alpha == 1:
-                        self.out("ctx.fillStyle = 'rgb(%d, %d, %d)';" % (r, g, b))
-                    else:
-                        self.out("ctx.fillStyle = 'rgba(%d, %d, %d, %d)';" % (r, g, b, alpha))
-                else:
-                    self.out("ctx.fillStyle = 'rgb(%d, %d, %d)';" % (r, g, b))
+        if style.has_key("fill") and style["fill"] != "none":
+            alpha = 1
+            if style.has_key("fill-opacity"):
+                alpha = style["fill-opacity"]
+            self.out("ctx.fillStyle = " + self.set_color(style["fill"], alpha))
 
     def effect(self):
         """Applies the effect"""
-
+        
         self.out("ctx = canvas.getContext(\"2d\");")
 
         for nid, node in self.selected.iteritems():
@@ -115,9 +109,8 @@ class Canvas(inkex.Effect):
             self.out("\nctx.closePath();")
 
             #reseting globalAlpha value
-            if style.has_key("opacity"):
-                if int(style["opacity"]) < 1:
-                    self.out("ctx.globalAlpha = 1;")
+            if style.has_key("opacity") and float(style["opacity"]) < 1:
+                self.out("ctx.globalAlpha = 1;")
 
             #if node.tag == inkex.addNS("text", "svg"):
 
@@ -126,4 +119,4 @@ class Canvas(inkex.Effect):
         f.close()
 
 b = Canvas()
-b.effect()
+b.affect()
