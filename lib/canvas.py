d97840c55252eeed979fa3391e59c7c60fefed0d
Karlisson Bezerra <theanimage@gmail.com>
2011-07-03 19:16:43 -0300

    Adicionando encoding meta tag

--- lib/canvas.py
+++ lib/canvas.py
@@ -41,6 +41,7 @@ class Canvas:
         <!DOCTYPE html>
         <html>
         <head>
+		    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
             <title>Inkscape Output</title>
         </head>
         <body>
