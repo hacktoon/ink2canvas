3d370a69a5593e4c1916820263e9ddf94fb189f5
Karlisson Bezerra <macedobz@gmail.com>
2012-03-10 18:49:43 -0300

    Adding standalone script for testing and using without Inkscape

---
+++ standalone.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -#-
+
+"""
+This script runs Ink2Canvas without needing to open Inkscape
+"""
+
+from ink2canvas.main import Ink2Canvas
+import sys
+
+i2c = Ink2Canvas()
+
+#catch first argument
+try:
+    svg_input = sys.argv[1]
+except IndexError:
+    print "Provide a SVG file to be parsed.\n"
+    print "Usage: python standalone.py INPUT [OUTPUT]"
+    sys.exit()
+
+#catch optional second argument for output file
+try:
+    html_output = sys.argv[2]
+except IndexError:
+    html_output = "%s.html" % svg_input.replace(".svg", "")
+
+#creates a svg element tree
+i2c.parse(svg_input)
+
+#applies the extension effect
+i2c.effect()
+
+output_file = open(html_output, "w")
+#get the html code
+content = i2c.canvas.output()
+output_file.write(content.encode("utf-8"))
+output_file.close()
