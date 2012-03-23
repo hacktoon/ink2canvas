3d47228f5e2a0a96e502d07fd4d61be7e1a6cb01
bublecamp <bublecamp@gmail.com>
2012-03-23 16:41:21 -0300

    comecamos unit test do arquivo/classe canvas

--- test/canvas_test.py
+++ test/canvas_test.py
@@ -2,62 +2,62 @@ import sys
 import unittest
 
 sys.path.append('..')
-from ink2canvas.canvas import Canvas
+import Canvas
 
 class TestSequenceFunctions(unittest.TestCase):
-  def setUp(self):
-    self.canvas = Canvas()
-    self.canvas.code = ""
-    self.string_rgb = "FFBBAA"
-    self.rgb = [251,186,10]
+    def setUp(self):
+        self.canvas = Canvas()
+        self.canvas.code = ""
+        self.string_rgb = "FFBBAA"
+        self.rgb = [251, 186, 10]
 
-  def test_out(self):
-    text = 'ctx = canvas.getContext("2d");'
-    self.canvas.out(text)
-    self.assertTrue(self.canvas.code, text)
+    def test_out(self):
+        text = 'ctx = canvas.getContext("2d");'
+        self.canvas.out(text)
+        self.assertTrue(self.canvas.code, text)
 
-  def test_rgb(self):
-    r,g,b = self.canvas.rgb(self.string_rgb)
-    self.assertTrue(r,self.rgb[0])
-    self.assertTrue(g,self.rgb[1])
-    self.assertTrue(b,self.rgb[2])
+    def test_rgb(self):
+        r, g, b = self.canvas.rgb(self.string_rgb)
+        self.assertTrue(r, self.rgb[0])
+        self.assertTrue(g, self.rgb[1])
+        self.assertTrue(b, self.rgb[2])
 
-  def test_style_for_stroke_width(self):
-    dictionary = {"stroke-width":"3"}
-    self.canvas.set_style(dictionary)
-    self.assertTrue(self.canvas.code,"ctx.lineWidth = 3;")
+    def test_style_for_stroke_width(self):
+        dictionary = {"stroke-width":"3"}
+        self.canvas.set_style(dictionary)
+        self.assertTrue(self.canvas.code, "ctx.lineWidth = 3;")
 
-  def test_style_for_stroke_linecap(self):
-    dictionary = {"stroke-linecap":3}
-    self.canvas.set_style(dictionary)
-    self.assertTrue(self.canvas.code,"ctx.lineCap = 3;")
+    def test_style_for_stroke_linecap(self):
+        dictionary = {"stroke-linecap":3}
+        self.canvas.set_style(dictionary)
+        self.assertTrue(self.canvas.code, "ctx.lineCap = 3;")
 
-  def test_style_for_stroke_linejoin(self):
-    dictionary = {"stroke-linejoin":3}
-    self.canvas.set_style(dictionary)
-    self.assertTrue(self.canvas.code,"ctx.lineJoin = 3;")
+    def test_style_for_stroke_linejoin(self):
+        dictionary = {"stroke-linejoin":3}
+        self.canvas.set_style(dictionary)
+        self.assertTrue(self.canvas.code, "ctx.lineJoin = 3;")
+        
+    def test_style_for_stroke_miterlimit(self):
+        dictionary = {"stroke-miterlimit":3}
+        self.canvas.set_style(dictionary)
+        self.assertTrue(self.canvas.code, "ctx.miterLimit = 3;")
 
-  def test_style_for_stroke_miterlimit(self):
-    dictionary = {"stroke-miterlimit":3}
-    self.canvas.set_style(dictionary)
-    self.assertTrue(self.canvas.code,"ctx.miterLimit = 3;")
+    def test_style_for_opacity(self):
+        dictionary = {"opacity":0.5}
+        self.canvas.set_style(dictionary)
+        self.assertTrue(self.canvas.code, "ctx.globalAlpha = 3;")
+        
+    def test_style_for_stroke(self):
+        r, g, b = self.rgb
+        dictionary = {"stroke":self.string_rgb}
+        self.canvas.set_style(dictionary)
+        self.assertTrue(self.canvas.code, "ctx.strokeStyle = 'rgb(%d, %d, %d)';" % (r, g, b))
 
-  def test_style_for_opacity(self):
-    dictionary = {"opacity":0.5}
-    self.canvas.set_style(dictionary)
-    self.assertTrue(self.canvas.code,"ctx.globalAlpha = 3;")
-
-  def test_style_for_stroke(self):
-    r,g,b = self.rgb
-    dictionary = {"stroke":self.string_rgb}
-    self.canvas.set_style(dictionary)
-    self.assertTrue(self.canvas.code,"ctx.strokeStyle = 'rgb(%d, %d, %d)';" % (r,g,b))
-
-  def test_style_for_stroke_with_opacity(self):
-    r,g,b = self.rgb
-    dictionary = {"stroke":self.string_rgb, "fill-opacity": 0}
-    self.canvas.set_style(dictionary)
-    self.assertTrue(self.canvas.code,"ctx.strokeStyle = 'rgba(%d, %d, %d, %d)';" % (r,g,b,0))
+    def test_style_for_stroke_with_opacity(self):
+        r, g, b = self.rgb
+        dictionary = {"stroke":self.string_rgb, "fill-opacity": 0}
+        self.canvas.set_style(dictionary)
+        self.assertTrue(self.canvas.code, "ctx.strokeStyle = 'rgba(%d, %d, %d, %d)';" % (r, g, b, 0))
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
---
+++ test/canvas_unit_test.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 23, 2012
+
+@author: bublecamp
+'''
+import unittest
+from ink2canvas.canvas import Canvas
+
+class TestCanvas(unittest.TestCase):
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
