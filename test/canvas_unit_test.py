b554776c07c4bbd4fbc7dfc7d285336afa670737
bublecamp <bublecamp@gmail.com>
2012-03-28 14:30:47 -0300

    modificado os testes de unidade

--- test/canvas_unit_test.py
+++ test/canvas_unit_test.py
@@ -1,17 +1,29 @@
-'''
-Created on Mar 23, 2012
-
-@author: bublecamp
-'''
+import sys
 import unittest
+sys.path.append('..')
+
+from mockito.mockito import *
 from ink2canvas.canvas import Canvas
 
 class TestCanvas(unittest.TestCase):
+    def setUp(self):
+        self.canvas = Canvas(100.0, 200.0)
+        self.canvasWithContext = Canvas(100.0, 200.0, "foo")
+        self.canvas.code = []
+        self.string_rgb = "FFBBAA"
+        self.rgb = [251, 186, 10]
+        
+    def testBeginPathIfWritesRight(self):
+        self.canvas.beginPath()
+        self.assertEqual(self.canvas.code, ["\tctx.beginPath();\n"])
+        
+    def testBeginPathIfWritesRightWithNewCtx(self):
+        self.canvasWithContext.beginPath()
+        self.assertEqual(self.canvasWithContext.code, ["\tfoo.beginPath();\n"])
+        
+        
 
-    def testName(self):
-        pass
 
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+if __name__ == '__main__':
     unittest.main()
+    
