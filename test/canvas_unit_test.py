8a2a90cbf94139a0f25e904730b711ea75fab4f6
Gustavo <gustavo@coelho-VirtualBox.(none)>
2012-03-28 16:26:30 -0300

    Pulled changes from master

--- test/canvas_unit_test.py
+++ test/canvas_unit_test.py
@@ -21,8 +21,75 @@ class TestCanvas(unittest.TestCase):
         self.canvasWithContext.beginPath()
         self.assertEqual(self.canvasWithContext.code, ["\tfoo.beginPath();\n"])
         
+    def testWriteCorrectInsertion(self):
+        text = "ctx.Texto"
+        self.canvas.write(text)
+        self.assertEqual(self.canvas.code[0], "\t" + text + "\n")
+    
+    def testWriteCorrectInsertionWithNewCtx(self):
+        text = "ctx.Texto"
+        self.canvasWithContext.write(text)
+        self.assertEqual(self.canvasWithContext.code[0], "\t" + text.replace("ctx", self.canvasWithContext.obj) + "\n")
+    
+    def testOutput(self):
+        from textwrap import dedent
+        output = self.canvas.output()
+        expected_output = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <title>Inkscape Output</title>
+        </head>
+        <body>
+            <canvas id='canvas' width='%d' height='%d'></canvas>
+            <script>
+            var %s = document.getElementById("canvas").getContext("2d");
+            %s
+            </script>
+        </body>
+        </html>
+        """
+        expected_output = dedent(expected_output) % (self.canvas.width, self.canvas.height, self.canvas.obj, "".join(self.canvas.code))
+        self.assertEqual(output, expected_output)
+    
+    def testCreateLinearGradient(self):
+        href = "str"
+        x1, y1, x2, y2 = 0.0 , 2.0 , 3.0, 4.0
+        data = (href, x1, y1, x2, y2)
+        expectedList = ["\tvar %s = \
+                   ctx.createLinearGradient(%f,%f,%f,%f);\n" % data]
+        self.canvas.createLinearGradient(href,x1, y1, x2, y2)
+        self.assertEqual(self.canvas.code, expectedList)          
+    
+    def testCreateRadialGradient(self):
+        href = "str"
+        cx1, cy1, rx, cx2, cy2, ry = 0.0 , 2.0, 3.0, 4.0, 5.0, 6.0
+        data = (href, cx1, cy1, rx, cx2, cy2, ry)
+        expectedList = ["\tvar %s = ctx.createRadialGradient\
+                   (%f,%f,%f,%f,%f,%f);\n" % data]
+        self.canvas.createRadialGradient(href, cx1, cy1, rx, cx2, cy2, ry)
+        self.assertEqual(self.canvas.code, expectedList)
+    
+    def testAddColorStop(self):
+        href, pos, color = "href" , 2.0, "color"
+        data = (href, pos, color)
+        expectedList = ["\t%s.addColorStop(%f, %s);\n" % data]
+        self.canvas.addColorStop(href, pos, color)
+        self.assertEqual(self.canvas.code, expectedList)
+           
+    def testSetOpacity(self):
+        #Float Test
+        value = 2.5
+        expectedReturn = "\tctx.globalAlpha = %.1f;\n" % float(value)
+        self.canvas.setOpacity(value)
+        self.assertEqual(self.canvas.code[0], expectedReturn)
         
-
+        #Integer Test
+        value = 2
+        expectedReturn = "\tctx.globalAlpha = %.1f;\n" % float(value)
+        self.canvas.setOpacity(value)
+        self.assertEqual(self.canvas.code[1], expectedReturn)
 
 if __name__ == '__main__':
     unittest.main()
