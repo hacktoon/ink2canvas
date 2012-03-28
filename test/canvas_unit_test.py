d4a61179c2766e6f1bb8eee107ef3152f2ae545d
bublecamp <bublecamp@gmail.com>
2012-03-28 16:21:44 -0300

    Continuacao do teste da funcao equalStyle

--- test/canvas_unit_test.py
+++ test/canvas_unit_test.py
@@ -8,11 +8,13 @@ from ink2canvas.canvas import Canvas
 class TestCanvas(unittest.TestCase):
     def setUp(self):
         self.canvas = Canvas(100.0, 200.0)
+        #testar CanvasWithContext qdo tiver write na funcao
         self.canvasWithContext = Canvas(100.0, 200.0, "foo")
         self.canvas.code = []
         self.string_rgb = "FFBBAA"
         self.rgb = [251, 186, 10]
         
+    #BeginPath()    
     def testBeginPathIfWritesRight(self):
         self.canvas.beginPath()
         self.assertEqual(self.canvas.code, ["\tctx.beginPath();\n"])
@@ -21,7 +23,17 @@ class TestCanvas(unittest.TestCase):
         self.canvasWithContext.beginPath()
         self.assertEqual(self.canvasWithContext.code, ["\tfoo.beginPath();\n"])
         
+    #EqualStyle
+    def testEqualStyleIfKeyIsInStyleCache(self):
+        self.canvas.styleCache = {'ola':1}
+        self.assertEqual(self.canvas.equalStyle(0,'ola'), True)
+
+    def testEqualStyleIfKeyIsNOTInStyleCache(self):
+        self.canvas.styleCache = {'ola':1}
+        self.assertEqual(self.canvas.equalStyle({'olaa':1},'olaa'), False)
         
+      
+   
 
 
 if __name__ == '__main__':
