4dbd5cd15b6072ca29b13c62fe1eaf721c00cf37
= <=>
2012-04-18 13:47:54 -0300

    Teste Circle

--- ink2canvas/svg/Circle.py
+++ ink2canvas/svg/Circle.py
@@ -2,7 +2,7 @@ from ink2canvas.svg.AbstractShape import AbstractShape
 
 class Circle(AbstractShape):
     def __init__(self, command, node, ctx):
-        AbstractShape(self, command, node, ctx)
+        AbstractShape(command, node, ctx)
         self.command = "arc"
 
     def get_data(self):
