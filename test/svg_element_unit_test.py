25594a13cfbd42cb3a0fa67c9adcc989e21b9684
= <=>
2012-04-18 14:41:17 -0300

    Correção do Test do Element

--- test/svg_element_unit_test.py
+++ test/svg_element_unit_test.py
@@ -16,13 +16,13 @@ class TestSvgElement(unittest.TestCase):
     def testAttrWithNs(self):
         self.element.node = self.node
         retorno = self.element.attr("width", "ns")
-        self.assertEqual(retorno, "210mm")
+        self.assertEqual(retorno, "12cm")
         
         
     def testAttrWithoutNs(self):
         self.element.node = self.node
         retorno = self.element.attr("width")
-        self.assertEqual(retorno, "210mm")
+        self.assertEqual(retorno, "12cm")
    
 if __name__ == '__main__':
     unittest.main()
