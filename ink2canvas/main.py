c5465d12f51120a549b5bd80dc246888ba2c3e76
= <=>
2012-04-27 15:03:20 -0300

    Refactory Main

--- ink2canvas/main.py
+++ ink2canvas/main.py
@@ -18,111 +18,19 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 '''
 
-import inkex
-from canvas import Canvas
-import svg
 import sys
-
-log = inkex.debug  #alias to debug method
-
+from Ink2CanvasCore import Ink2CanvasCore
  
-class Ink2Canvas(inkex.Effect):
-    def __init__(self):
-        inkex.Effect.__init__(self)
-        self.canvas = None
+class Ink2Canvas():
+    def __init__(self):   
+        self.core = Ink2CanvasCore()
 
     def output(self):
         content = self.canvas.output()
         sys.stdout.write(content.encode("utf-8"))
 
-    def getNodeTagName(self, node):
-        # remove namespace part from "{http://www.w3.org/2000/svg}elem"
-        return node.tag.split("}")[1]
-
-    def getGradientDef(self, elem):
-        if not elem.has_gradient():
-            return None
-        gradientHref = elem.get_gradient_href()
-        
-        # get the gradient element
-        gradient = self.xpathSingle("//*[@id='%s']" % gradientHref)
-        
-        # get the color stops
-        colorStops = gradient.get(inkex.addNS("href", "xlink"))
-        
-        colorStopsNodes = self.xpathSingle("//svg:linearGradient[@id='%s']" % colorStops[1:])
-        
-        colors = []
-        for color in colorStopsNodes:
-            colors.append(color.get("style"))
-        if gradient.get("r"):
-            return svg.RadialGradientDef(gradient, colors)
-        else:
-            return svg.LinearGradientDef(gradient, colors)
-
-    def getClipDef(self, elem):
-        clipId = elem.get_clip_href()
-        return self.xpathSingle("//*[@id='%s']" % clipId)
-
-    def isCloneNode(self, node):
-        cloneHref = node.get(inkex.addNS("cloneHref", "xlink"))
-        return bool(cloneHref)
-
-    def getCloneNode(self, node):
-        cloneHref = node.get(inkex.addNS("cloneHref", "xlink"))
-        clone = self.xpathSingle("//*[@id='%s']" % cloneHref[1:])
-        return clone
-
-    def walkInSVGNodes(self, rootNode, isClip=False):
-        for childNode in rootNode:
-            tagName = self.getNodeTagName(childNode)
-            className = tagName.capitalize()
-
-            #if there's not an implemented class, continues
-            if not hasattr(svg, className):
-                continue
-            # creates a instance of 'element'
-            # similar to 'element = Rect(tagName, childNode, ctx)'
-            element = getattr(svg, className)(tagName, childNode, self.canvas)
-            
-            if self.isCloneNode(childNode):
-                cloneNode = self.getCloneNode(childNode)
-                if (element.has_transform()):
-                    transMatrix = element.get_transform()
-                    self.canvas.transform(*transMatrix)
-                self.walkInSVGNodes([cloneNode])
-                continue
-            
-            gradient = self.getGradientDef(element)
-            element.start(gradient)
-            
-            #render only the 'first level' elements in a clipping area
-            if not isClip and element.has_clip():
-                clipPath = self.getClipDef(element)
-                self.canvas.beginPath()
-                if (element.has_transform()):
-                    self.canvas.save()
-                    transMatrix = element.get_transform()
-                    self.canvas.transform(*transMatrix)
-                self.walkInSVGNodes(clipPath, True)
-                if (element.has_transform()):
-                    self.canvas.restore()
-                self.canvas.clip()
-            
-            #clipping elements are drawn differently
-            element.draw(isClip)
-            self.walkInSVGNodes(childNode, isClip)
-            element.end()
-
-    def effect(self):
-        """Applies the effect"""
-        svgRoot = self.document.getroot()
-        width = inkex.unittouu(svgRoot.get("width"))
-        height = inkex.unittouu(svgRoot.get("height"))
-        self.canvas = Canvas(width, height)
-        self.walkInSVGNodes(svgRoot)
-
-
+    
 if __name__ == "__main__":
-    ink = Ink2Canvas()
-    ink.affect()
+    i2c = Ink2Canvas()
+    i2c.core.affect()
+
