4dec0c3f0a4436324f2326f9ef3edd88de7922e9
bublecamp <bublecamp@gmail.com>
2012-05-25 14:40:32 -0300

    Fizemos o setup do unit test gradient

---
+++ test/arquivos_test/svg_linearGradient_unit_test.py
@@ -0,0 +1,27 @@
+import sys
+import unittest
+from inkex import Effect
+sys.path.append('..')
+
+from ink2canvas.svg.LinearGradient import Lineargradient
+
+class LinearGradientTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.effect = Effect()
+        self.document = self.effect.parse("arquivos_test/linearGradient.svg")
+        root = self.effect.document.getroot()
+        
+        for node in root:
+            tag = node.tag.split("}")[1]
+            if(tag == 'linearGradient'):
+                self.node = node
+                break   
+        self.linearGradient = Lineargradient(None, self.node, self.canvas, None)
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
