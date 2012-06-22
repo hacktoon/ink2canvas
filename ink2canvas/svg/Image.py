299c812b90d1c2a15ad1bf88e96bcb9c39d27484
bublecamp <bublecamp@gmail.com>
2012-06-22 16:01:10 -0300

    image.py terminado e unit testes da mesma incompleta.

--- ink2canvas/svg/Image.py
+++ ink2canvas/svg/Image.py
@@ -10,7 +10,7 @@ class Image(AbstractShape):
         height = self.attr("height")
         href = self.attr("href","xlink")
         return x, y, width, height, href
-
+    
     def draw(self, isClip=False):
         if self.has_transform():
             trans_matrix = self.get_transform()
@@ -21,7 +21,7 @@ class Image(AbstractShape):
             self.ctx.beginPath()
 
         x, y, width, height, href = self.get_data()
-
+        
         self.ctx.write("\n\tvar image = new Image();")
         self.ctx.write("\n\timage.src = '" + href +"';")
         self.ctx.write("\n\tctx.drawImage(image, %f,%f);" %(x,y))
@@ -29,7 +29,6 @@ class Image(AbstractShape):
         if not isClip: 
             self.ctx.closePath()
         
-        if self.has_transform() or self.hasClip():
-            self.ctx.save()
+        
 
 
